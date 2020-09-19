26875458ef40d94f8d717720296a8db532198e84
Elio Osés <elio@theaterix.com>
2020-09-19 00:35:56 -0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,113 @@
+<template>
+  <div id="app">
+    <!--<img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+<div v-for="item in items" :key="item.id" data-aos="zoom-out-up" data-aos-offset="100" data-aos-easing="ease-out-back" class="col-lg-4 my-4">
+            <div class="col-lg-4 col-sm-6">
+                    <div v-if="item.type === 'media' && statusVideo === true" class="popup" @click="statusVideo = false">
+                      <youtube :video-id="thing" ref="youtube" @playing="playing" :player-vars="playerVars"></youtube>
+                    </div>
+                    <figure>
+                        <img @click="doSomethingWith(item.url); playVideo" v-if="item.cover" :src="item.cover" alt="" style="max-width: 100%; width: 100%; height: auto;">
+                    </figure>
+                    <div class="gallery-box-caption">
+                        <div class="gallery-box-content">
+                            <div>
+                                <i class="icon-lg ion-ios-play"></i>
+                            </div>
+                        </div>
+                    </div>
+                    {{ item.type }}
+                    <h1 class="text-primary talk-title">{{ item.title }}</h1>
+            </div>
+        </div> 
+        
+  
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import Vue from 'vue'
+import VueYoutube from 'vue-youtube'
+
+Vue.use(VueYoutube)
+
+const api = 'https://theaterix.com/api/posts.json'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  },
+  data: function () {
+    return {
+      page: 1,
+      items: [],
+      thing: '',
+      statusVideo: false,
+      playerVars: {
+        autoplay: 1
+      }           
+    }
+  }, 
+  methods: {
+    doSomethingWith(item) {
+        this.thing = item;
+        this.statusVideo = true;
+    },    
+    api () {
+      // Sends a get request to the API endpoint.
+      axios.get(api).then(response => {
+          this.items = response.data
+          console.log(this.items);
+      });
+    },
+    playVideo() {
+      this.player.playVideo()
+    },   
+    playing() {
+      console.log('/ we are watching!!!')
+    }    
+  },
+  created() {
+    this.api();
+  },
+  computed: {
+    player() {
+      return this.$refs.youtube.player
+    }
+  }     
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+body {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+figure {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+.popup {
+  width: 100%;
+  height: 100vh;
+  position: fixed;
+  background: #000000;
+  z-index: 1;
+}
+</style>
